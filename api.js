f8fdeee823f90d42800b3e32141681fab33a53c1
kelvinrlq <kelvinrx00@gmail.com>
2025-04-22 21:03:56 -0400

    implementada a navegacao

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const port = 3333
 const api = axios.create({
-    baseURL: "http://localhost:" + port,
+    baseURL:"http:/localhost" + port,
 });
 
 export default api;
